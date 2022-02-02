4acffc3aa6210ee522334ee907d119642e9f2454
mayuryande <mayuryande3122@gmail.com>
2022-02-02 17:09:40 +0530

    adding a new project

---
+++ com.HybridFramework/src/main/java/com/base/BaseClass.java
@@ -0,0 +1,19 @@
+package com.base;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+	
+	public static WebDriver Driver=null;
+
+
+public void initiliaztion() {
+	
+	//String PropertiesUtils).readproperty("browser");
+
+}
+
+
+}
---
+++ com.HybridFramework/src/main/java/com/utility/PropertyUtility.java
@@ -0,0 +1,26 @@
+
+package com.utility;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertyUtility {
+	public static  String readproperties(String key) {
+		Properties prop= new Properties ();
+		try {
+		FileInputStream fis= new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/config.properties");
+		prop.load(fis);
+		
+		}
+		catch (Exception e ) {
+			e.printStackTrace();
+			
+		}
+		return prop.getProperty(key);
+ 		
+	}
+
+	public static void main(String[] args) {
+		System.out.println((System.getProperty("user.dir")+"/src/main/resources/config.properties"));
+	}
+}
